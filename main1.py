1c862f7e7c26c29bcdf59735045a349daf26d964
Willi Akbar <willysatria208@gmail.com>
2021-12-07 02:49:06 +0700

    initial commit

---
+++ main1.py
@@ -0,0 +1,14 @@
+# percabangan
+jumlah_botol_susu = 65
+jumlah_telur = 100
+print(f"jumlah botol susu {jumlah_botol_susu} btl")
+print (f"jumlah telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print("budi mengecek uangnya, maka uangnya cukup")
+    print("budi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli 1 botol susu")
+
+print("budi pulang kerumah")
+print("menyampaikan hasilnya kepada ibu")
